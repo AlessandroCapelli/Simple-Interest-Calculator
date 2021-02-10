604f4c72d9c38276de99e78302192fa7bfab8e90
Alessandro Capelli <me@alessandrocapelli.it>
2021-02-10 23:21:13 +0000

    update files

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+// compute function
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
-}
+    // retrieve elements
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    // check params
+    if(parseFloat(principal) <= 0 || principal == "") {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    // calculate interest
+    else {
+        interest = principal * years * rate / 100;
+
+        var tempY = new Date();
+        tempY = tempY.getFullYear();
+        tempY = tempY + parseInt(years);
         
+        document.getElementById("result").innerHTML = "If you deposit " + principal + "<br>at an interest rate of " + rate + "%.<br>You will receive an amount of " + interest + ",<br>in the year " + tempY;
+    }
+}
+
+// update rate label function
+function updateRateLabel() {
+    rate = document.getElementById("rate").value;
+
+    document.getElementById("rateLabel").innerHTML = rate + " %";
+}
